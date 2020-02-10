d4e6264bbf1a73b0ca632b8e55ddd5629b8a7bf5
liulei <liulei13041x@163.com>
2020-02-10 17:05:07 +0800

    Initial commit

---
+++ src/main/java/com/test/service3/controller/Consumer.java
@@ -0,0 +1,47 @@
+package com.test.service3.controller;
+
+import com.test.service3.entity.User;
+import com.test.service3.utils.MSendRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Lei
+ * @Date 2020/2/8 23:34
+ *
+ * @Version 1.0
+ */
+
+@RequestMapping("/consumer")
+@RestController
+public class Consumer {
+
+    @Autowired
+    private MSendRequest mSendRequest;
+
+
+
+    @GetMapping("/get1")
+    public User f(){
+        return mSendRequest.sendRequest("http://gateway/service2/provider/getTest","1.0.0", User.class, RequestMethod.GET);
+    }
+    @GetMapping("/get2")
+    public User as(){
+        return mSendRequest.sendRequest("http://gateway/service2/provider/getTest", User.class, RequestMethod.GET);
+    }
+    @GetMapping("/get3")
+    public User asa(){
+        User user = new User();
+        user.setAge(1);
+        user.setName("adf");
+        user.setUsername("asdasa");
+        return mSendRequest.sendRequest("http://gateway/service2/provider/postTest", User.class, RequestMethod.POST,user);
+    }
+
+
+
+}
---
+++ src/main/java/com/test/service3/controller/provider.java
@@ -0,0 +1,32 @@
+package com.test.service3.controller;
+
+import com.test.service3.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author Lei
+ * @Date 2020/2/9 16:43
+ * @Version 1.0
+ */
+
+@RestController
+@RequestMapping("/provider")
+public class provider {
+
+    @Value("${eureka.instance.metadata-map.version}")
+    private String version;
+    @GetMapping("/getServiceInfo")
+    public String getVersion(){
+        return this.version;
+    }
+
+    @GetMapping("/get/{id}")
+    public User getUser(@PathVariable("id")int id){
+        User a = new User();
+        a.setAge(id);
+        a.setName("service3");
+        return a;
+    }
+}
